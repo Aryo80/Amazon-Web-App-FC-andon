fac9df44e561f989bf7bd160598722d30bd88904
Mahmood Honarvar <55058593+Aryo80@users.noreply.github.com>
2023-12-06 19:36:17 -0500

    Amazon.py

--- Amazon.py
+++ Amazon.py
@@ -117,6 +117,7 @@ col1, col2 = st.columns((2))
 with col1:
     st.plotly_chart(fig,use_container_width=True)
 with col2:
+    st.subheader('Total number of Andons =  ' +str(len(data) ) )
     st.table(area)
     
 # Line chart using Plotly Express
