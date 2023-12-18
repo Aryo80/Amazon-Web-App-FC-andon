72f82c2a2d909a4d5a4631a42089bebf58bc81de
Mahmood Honarvar <55058593+Aryo80@users.noreply.github.com>
2023-12-18 10:55:04 -0500

    Update Amazon.py

--- Amazon.py
+++ Amazon.py
@@ -68,8 +68,11 @@ def generate_simulated_data(num_rows=1000):
     })
     df = pd.DataFrame(data)
     return df
-
-file = st.file_uploader("Upload /Drag and Drop CSV file from FC ANDON", type=['csv'])
+try:
+    file = st.file_uploader("Upload /Drag and Drop CSV file from FC ANDON", type=['csv'])
+except Exception as e:
+    st.error(f"An error occurred: {e}")
+    
 if file is not None:
     # Read the uploaded file with Pandas
     data= pd.read_csv(file) 
