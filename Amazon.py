018b6e270f4f7efef60ca754ff459e66566d6d70
Mahmood Honarvar <55058593+Aryo80@users.noreply.github.com>
2023-12-19 09:48:20 -0500

    Update Amazon.py

--- Amazon.py
+++ Amazon.py
@@ -187,7 +187,7 @@ def ps_report():
          #
 
     st.subheader(report_ps)
-    col1, col2= st.columns([2, 2])
+    col1, col2= st.columns([3, 2])
     with col1:
          st.write(ps_ind_data)
     with col2:
@@ -196,10 +196,10 @@ def ps_report():
          st.write(f'{total_rec_ind} Andon has been resolved by {report_ps}')
          schadual_ps = df_sorted[df_sorted['Assigned to'] == report_ps]['Last updated date'].unique()
          ind_scheduele = st.radio('Scheduele in Details ',schadual_ps)
-         st.write(df_sorted[(df_sorted['Assigned to'] == report_ps) & (df_sorted['Last updated date'] == ind_scheduele)]
+    st.write(df_sorted[(df_sorted['Assigned to'] == report_ps) & (df_sorted['Last updated date'] == ind_scheduele)]
             .groupby('Type')
             .size()
-            .reset_index(name='counts')) 
+            .reset_index(name='counts'))      
             
 def hot_bin_report():
     st.header('hot_bin_report')
