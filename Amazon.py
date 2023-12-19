cfb1b7b4342892f31fa65bb11c2296a14e997786
Mahmood Honarvar <55058593+Aryo80@users.noreply.github.com>
2023-12-19 10:13:40 -0500

    Update Amazon.py

--- Amazon.py
+++ Amazon.py
@@ -208,7 +208,7 @@ def ps_report():
      st.write(df_sorted[(df_sorted['Assigned to'] == report_ps) & (df_sorted['Last updated date'] == ind_scheduele)]
                 .groupby('Type')
                 .size()
-                .reset_index(name='counts').sum()      
+                .sum()      
                        
 def hot_bin_report():
     st.header('hot_bin_report')
